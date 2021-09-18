2520b38b3a52f14f2584603d0536f8f34f5eecd3
Khaldoon Sinjab <khaldoon@sinjab.com>
2021-09-18 22:27:22 +0400

    hashing

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -27,17 +27,69 @@ Blockchain.blocks.push({
 	timestamp: Date.now(),
 });
 
-// TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
+// insert each line into blockchain
+for (let line of poem) {
+	Blockchain.blocks.push(
+		createBlock(line)
+	)
+}
+console.log("Blocks");
 
-// console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
+console.table(Blockchain.blocks, ['hash', 'prevHash']);
+console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
 
+console.table(Blockchain.blocks, ['hash', 'data']);
+let i = 7
+Blockchain.blocks[i].data = "the power of wind can chill."
+Blockchain.blocks[i].hash = blockHash(Blockchain.blocks[i])
+Blockchain.blocks[i+1].prevHash = Blockchain.blocks[i].hash
+console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
 
 // **********************************
 
 function blockHash(bl) {
 	return crypto.createHash("sha256").update(
-		// TODO: use block data to calculate hash
+		bl.data
 	).digest("hex");
+}
+
+// function which takes the text for its data, creates an object for the block, and computes its hash, finally returning the block object
+function createBlock(text) {
+	var obj = {};
+	obj.index = Blockchain.blocks[Blockchain.blocks.length - 1].index + 1;
+	obj.prevHash = Blockchain.blocks[Blockchain.blocks.length - 1].hash;
+	obj.data = text;
+	obj.timestamp = Date.now();
+	obj.hash = blockHash(obj);
+
+	return obj;
+}
+
+// checks all blocks in the chain to ensure the chain is valid, and returns true or false accordingly
+function verifyChain(bc) {
+	for (let i = 1; i < bc.blocks.length; i++) {
+		if (!verifyBlock(bc.blocks[i]))
+			return false
+		if (bc.blocks[i-1].hash != bc.blocks[i].prevHash)
+			return false
+	}
+
+	return true
+}
+
+// checks a block to ensure that it is valid, and returns true or false accordingly
+function verifyBlock(bl) {
+	if (bl.index == 0 && bl.hash == "000000")
+		return true
+
+	if (bl.index < 0)
+		return false
+
+	if (bl.data == "")
+		return false
+
+	if (bl.hash != blockHash(bl))
+		return false
+
+	return true
 }
